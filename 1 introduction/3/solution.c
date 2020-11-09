71eb639dc180803462865182f116677e5adceabe
valeria <leradopira@mail.ru>
2020-11-09 18:44:58 +0300

    renamed folders

--- 1 introduction/3/solution.c
+++ 1 introduction/3/solution.c
@@ -1,5 +1,4 @@
 #include <linux/module.h>
-#include <stdlib.h>
 #include "checker.h"
 
 int count_sum(short *arr, size_t n) {
@@ -25,36 +24,25 @@ void result_from_array_sum(short *arr) {
 	}
 }
 
-short generate_array(int n)
-{
-	short my_array[n];
-	int i;
-	for (i = 0; i < n; i++) {
-	    my_array[i] = rand();
-	}
-	return my_array;
-}
-
-
 int init_mdl(void) {
 	CHECKER_MACRO;
 	printk(KERN_INFO "kernel_mooc: init\n");
 
-	short arr1 = generate_array(10);
-	short arr2 = generate_array(5);
-	short arr3 = generate_array(11);
-	short arr4 = generate_array(8);
-	short arr5 = generate_array(5);
-	short arr6 = generate_array(10);
-	short arr7 = generate_array(15);
-	short arr8 = generate_array(9);
-	short arr9 = generate_array(7);
-	short arr10 = generate_array(20);
-	short arr11 = generate_array(12);
-	short arr12 = generate_array(13);
-	short arr13 = generate_array(14);
-	short arr14 = generate_array(15);
-	short arr15 = generate_array(16);
+	short arr1[] = {1, 2, 3, 4, 5};
+	short arr2[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+	short arr3[] = {9, 8, 7, 6, 5, 4};
+	short arr4[] = {3, 2, 1, 9, 8, 7, 4, 5};
+	short arr5[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+	short arr6[] = {1, 9, 9, 1, 1, 9, 9, 1, 1, 9};
+	short arr7[] = {2, 5, 3, 2, 5, 3, 2, 5, 3, 2, 5, 3, 2, 5, 3};
+	short arr8[] = {4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9};
+	short arr9[] = {3, 2, 4, 3, 1, 2};
+	short arr10[] = {6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6};
+	short arr11[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	short arr12[] = {9, 8, 7, 6, 5, 4, 1, 1, 1, 1};
+	short arr13[] = {1, 2};
+	short arr14[] = {0, 1, 9, 8, 7, 6, 5, 5, 3, 3, 3, 3};
+	short arr15[] = {5, 4, 1, 1, 1, 1, 5, 4, 2, 7, 2, 1, 3, 0};
 	
 	result_from_array_sum(arr1);
 	result_from_array_sum(arr2);
